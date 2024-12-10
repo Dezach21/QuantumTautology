19f51d9a531f44331e2fb636019d030adb5a1c3f
Dezach21 <dezach2121@gmail.com>
2024-12-10 01:07:51 -0500

    More gates

--- gate.py
+++ gate.py
@@ -1,91 +1,120 @@
-from math import *
-from matrix import *
+from QuantumTautology.math import PI, sqrt, exp, sin, cos
+from QuantumTautology.matrix import Matrix, identity_matrix, embed
 
 
 class QuantumGates(Matrix):
-    # --- Pauli gates --- #
+    # --- Identity gates --- #
 
     @staticmethod
-    def gate_x() -> Matrix:
+    def i() -> Matrix:
+        """Identity gate."""
+        return identity_matrix(2)
+
+    @staticmethod
+    def ph(angle: int | float) -> Matrix:
+        """
+        Global phase gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        return identity_matrix(2) * exp(1j * angle)
+
+    # --- Clifford qubit gates --- #
+
+    # - Pauli - #
+
+    @staticmethod
+    def x() -> Matrix:
         """Pauli X gate."""
         return Matrix([[0, 1],
                        [1, 0]])
 
     @staticmethod
-    def gate_y() -> Matrix:
+    def y() -> Matrix:
         """Pauli Y gate."""
-        return Matrix([[0, -1j],
-                       [1j, 0]])
+        return Matrix([[0, -1],
+                       [1, 0]]) * 1j
 
     @staticmethod
-    def gate_z() -> Matrix:
+    def z() -> Matrix:
         """Pauli Z gate."""
         return Matrix([[1, 0],
                        [0, -1]])
 
-    # --- Controlled gates --- #
+    # - Pauli square roots - #
 
     @staticmethod
-    def gate_cx() -> Matrix:
+    def sx() -> Matrix:
+        """Square root of X."""
+        return Matrix([[1 + 1j, 1 - 1j],
+                       [1 - 1j, 1 + 1j]]) / 2
+
+    @staticmethod
+    def sy() -> Matrix:
+        """Square root of Y."""
+        return Matrix([[0, 1 - 1j],
+                       [1 + 1j, 0]]) / sqrt(2)
+
+    @staticmethod
+    def sz() -> Matrix:
+        """Square root of Z."""
+        return Matrix([[1, 0],
+                       [0, 1j]])
+
+    # - Hadamard - #
+
+    @staticmethod
+    def h() -> Matrix:
+        """Hadamard gate."""
+        return Matrix([[1, 1],
+                       [1, -1]]) / sqrt(2)
+
+    # - Controlled gates - #
+
+    @staticmethod
+    def cx() -> Matrix:
         """Controlled not gate."""
-        return Matrix([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
-                       [0, 0, 0, 1],
-                       [0, 0, 1, 0]])
+        return embed(QuantumGates.x(), 4)
 
     @staticmethod
-    def gate_cy() -> Matrix:
-        """Controlled Y gate."""
-        return Matrix([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
-                       [0, 0, 0, -1j],
-                       [0, 0, 1j, 0]])
-
-    @staticmethod
-    def gate_cz() -> Matrix:
-        """Controlled Z gate."""
-        return Matrix([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
+    def acx() -> Matrix:
+        """Anti-controlled not gate."""
+        return Matrix([[0, 1, 0, 0],
+                       [1, 0, 0, 0],
                        [0, 0, 1, 0],
-                       [0, 0, 0, -1]])
+                       [0, 0, 0, 1]])
 
     @staticmethod
-    def gate_cu(operation_gate: Matrix) -> Matrix:
+    def dcx() -> Matrix:
+        """Double-controlled not gate."""
+        return Matrix([[1, 0, 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 0, 0, 1],
+                       [0, 1, 0, 0]])
+
+    @staticmethod
+    def cy() -> Matrix:
+        """Controlled Y gate."""
+        return embed(QuantumGates.y(), 4)
+
+    @staticmethod
+    def cz() -> Matrix:
+        """Controlled Z gate."""
+        return embed(QuantumGates.z(), 4)
+
+    @staticmethod
+    def cu(operation_gate: Matrix) -> Matrix:
         """
         Controlled U gate.
 
         :param operation_gate: `U` gate that operates on a single qubit.
         """
-        return Matrix([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
-                       [0, 0, operation_gate[0][0], operation_gate[0][1]],
-                       [0, 0, operation_gate[1][0], operation_gate[1][1]]])
+        return embed(operation_gate, 4)
 
-    # --- Other gates --- #
+    # - Swap gates - #
 
     @staticmethod
-    def gate_i() -> Matrix:
-        """Identity gate."""
-        return identity_matrix(2)
-
-    @staticmethod
-    def gate_p(angle: float) -> Matrix:
-        """
-        Phase gate.
-
-        :param angle: Phase shift angle in radians.
-        """
-        return Matrix([[1, 0],
-                       [0, exp(1j * angle)]])
-
-    @staticmethod
-    def gate_h() -> Matrix:
-        """Hadamard gate."""
-        return Matrix([[1 / sqrt(2), 1 / sqrt(2)],
-                       [1 / sqrt(2), -1 / sqrt(2)]])
-
-    @staticmethod
-    def gate_s() -> Matrix:
+    def s() -> Matrix:
         """Swap gate."""
         return Matrix([[1, 0, 0, 0],
                        [0, 0, 1, 0],
@@ -93,9 +122,122 @@ class QuantumGates(Matrix):
                        [0, 0, 0, 1]])
 
     @staticmethod
-    def gate_ccx() -> Matrix:
-        """Toffoli gate."""
-        m = identity_matrix(8)
-        m[6][6], m[7][6], m[6][7], m[7][7] = 0, 1, 1, 0
+    def ims() -> Matrix:
+        """Imaginary swap gate."""
+        return Matrix([[1, 0, 0, 0],
+                       [0, 0, 1j, 0],
+                       [0, 1j, 0, 0],
+                       [0, 0, 0, 1]])
 
-        return m
+    # --- Non-Clifford qubit gates --- #
+
+    # - Phase gates - #
+
+    @staticmethod
+    def p(angle: int | float) -> Matrix:
+        """
+        Phase shift gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        return embed(exp(1j * angle), 2)
+
+    @staticmethod
+    def cp(angle: int | float) -> Matrix:
+        """
+        Controlled phase gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        return embed(exp(1j * angle), 4)
+
+    @staticmethod
+    def csz() -> Matrix:
+        """Controlled square root of Z gate."""
+        return embed(1j, 4)
+
+    # - Rotation gates - #
+
+    @staticmethod
+    def rx(angle: int | float) -> Matrix:
+        """
+        Rotation about x-axis gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        theta = angle / 2
+        c, s = cos(theta), -1j * sin(theta)
+        return Matrix([[c, s],
+                       [s, c]])
+
+    @staticmethod
+    def ry(angle: int | float) -> Matrix:
+        """
+        Rotation about y-axis gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        theta = angle / 2
+        c, s = cos(theta), sin(theta)
+        return Matrix([[c, -s],
+                       [s, c]])
+
+    @staticmethod
+    def rz(angle: int | float) -> Matrix:
+        """
+        Rotation about z-axis gate.
+
+        :param angle: Phase shift angle in radians.
+        """
+        theta = angle / 2j
+        return Matrix([[exp(theta), 0],
+                       [0, exp(-theta)]])
+
+    # - Two-qubit interaction gates - #
+
+    # TODO xx, yy, zz, xy, etc.
+
+    # - Swap gates - #
+
+    @staticmethod
+    def ss() -> Matrix:
+        """Square root of swap gate."""
+        return Matrix([[2, 0, 0, 0],
+                       [0, 1 + 1j, 1 - 1j, 0],
+                       [0, 1 - 1j, 1 + 1j, 0],
+                       [0, 0, 0, 2]]) / 2
+
+    @staticmethod
+    def sims() -> Matrix:
+        """Square root of imaginary swap gate."""
+        return Matrix([[sqrt(2), 0, 0, 0],
+                       [0, 1, 1j, 0],
+                       [0, 1j, 1, 0],
+                       [0, 0, 0, sqrt(2)]]) / sqrt(2)
+
+    @staticmethod
+    def sp(power: int | float) -> Matrix:
+        """
+        Swap gate raised to power.
+
+        :param power: Power.
+        """
+        e = exp(-1j * PI * power)
+        return Matrix([[2, 0, 0, 0],
+                       [0, 1 + e, 1 - e, 0],
+                       [0, 1 - e, 1 + e, 0],
+                       [0, 0, 0, 2]]) / 2
+
+    @staticmethod
+    def cs() -> Matrix:
+        """Controlled swap gate, or Fredkin gate."""
+        return embed(QuantumGates.s(), 8)
+
+    # - Others - #
+
+    @staticmethod
+    def ccx() -> Matrix:
+        """Controlled controlled-not gate, or Toffoli gate."""
+        return embed(QuantumGates.x(), 8)
+
+    # TODO Add others.
